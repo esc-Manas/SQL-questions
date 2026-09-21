a749bdae40824e73862cdfeb2b28296f6e4ff124
esc-Manas <tiwarimanas8080@gmail.com>
2026-09-21 23:45:43 +0530

    problem1

---
+++ Problem1.sql
@@ -0,0 +1,19 @@
+-- Look for non-senior data analyst or business analyst roles     
+--  Only get job titles that include either 'Data' or 'Business'     
+--   Also include those with 'Analyst' in any part of the title     
+--    Don't include any job titles with 'Senior' followed by any character     
+--     ▶ Get the job title, location, and average yearly salary
+
+SELECT 
+     job_title, 
+     job_location AS location,
+     salary_year_avg AS salary
+FROM
+     job_postings_fact
+     
+WHERE
+     (job_title LIKE '%Data%' OR job_title LIKE '%Business%') 
+     AND
+     job_title LIKE '%Analyst%' 
+     AND
+     job_title NOT LIKE '%Senior%'
